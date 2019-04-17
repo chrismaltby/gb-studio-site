99ed14351ae1c9f1ba882e255b27cb64a53bfb96
Chris Maltby <chris@feed.xyz>
2019-04-17 16:49:40 +0100

    Don't show linux downloads when viewed on Android

--- themes/gbstudio/src/js/main.js
+++ themes/gbstudio/src/js/main.js
@@ -4,7 +4,7 @@ let OSName="Unknown OS";
 if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
 if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
 if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+if (navigator.appVersion.indexOf("Linux")!=-1 && navigator.appVersion.indexOf("Android")==-1) OSName="Linux";
 
 const downloadBtn = document.getElementById("download-btn");
 const downloadLink = document.getElementById("download-link");
