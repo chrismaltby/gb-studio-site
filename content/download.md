a08127e9694758035e77e34b25afdf48cb82b565
Chris Maltby <chris@feed.xyz>
2019-04-17 14:51:45 +0100

    Added download urls

--- content/download.md
+++ content/download.md
@@ -6,23 +6,22 @@ draft: false
 
 ## Windows
 * **Squirrel Installer**    
-  [GB Studio-win32-x64-squirrel-1.0.0.zip](#)  
+  [GB Studio-win32-x64-squirrel-1.0.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-win32-x64-squirrel-1.0.0.zip)  
   *Installs to C:\\ automatically*
 
 * **Manual Install Zip**  
-  [GB Studio-win32-x64-1.0.0.zip](#)  
+  [GB Studio-win32-x64-1.0.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-win32-x64-1.0.0.zip)  
 
 ## MacOS
 
 * **Application Package**  
-  [GB Studio-darwin-x64-1.0.0.zip](#)  
-
+  [GB Studio-darwin-x64-1.0.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-darwin-x64-1.0.0.zip)  
 ## Linux
 
 * **Ubuntu / Debian-based**  
-  [gb-studio_1.0.0_amd64.deb](#)
+  [gb-studio_1.0.0_amd64.deb](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/gb-studio_1.0.0_amd64.deb)
 
 * **Fedora / RPM-based**  
-  [gb-studio-1.0.0.x86_64.rpm](#)
+  [gb-studio-1.0.0.x86_64.rpm](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/gb-studio-1.0.0.x86_64.rpm)
 
 ## Next: [Installation](/docs/installation)
--- themes/gbstudio/src/js/main.js
+++ themes/gbstudio/src/js/main.js
@@ -9,20 +9,33 @@ if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 const downloadBtn = document.getElementById("download-btn");
 const downloadLink = document.getElementById("download-link");
 
+const downloadUrls = {
+  windows: "https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-win32-x64-squirrel-1.0.0.zip",
+  mac: "https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-darwin-x64-1.0.0.zip",
+  deb: "https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/gb-studio_1.0.0_amd64.deb",
+  rpm: "https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/gb-studio-1.0.0.x86_64.rpm" 
+}
+
 if(downloadBtn) {
   switch(OSName) {
     case "Windows":
       downloadBtn.innerHTML = "Download for Windows";
+      downloadBtn.href = downloadUrls.windows;
       break;
     case "MacOS":
       downloadBtn.innerHTML = "Download for macOS";
+      downloadBtn.href = downloadUrls.mac;      
       break;    
     case "Linux":
       downloadBtn.innerHTML = "Download for Ubuntu";
+      downloadBtn.href = downloadUrls.deb;      
       const redhatDownloadBtn = document.createElement("a");
       redhatDownloadBtn.className = "homepage-landing__button";
       redhatDownloadBtn.innerHTML = "Download for Redhat"
+      redhatDownloadBtn.href = downloadUrls.rpm;      
       downloadBtn.parentNode.insertBefore(redhatDownloadBtn, downloadLink);
-      break;            
+      break;   
+    default:
+      downloadBtn.innerHTML = "Download GB Studio";               
   }
 }
