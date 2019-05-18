945d239661fd012404f41c5c8f5a414ea6c6800f
Chris Maltby <chris.maltby@gmail.com>
2019-05-18 11:52:14 +0100

    Added 1.1.0 downloads

--- content/download.md
+++ content/download.md
@@ -4,27 +4,36 @@ date: 2018-04-29T14:14:44Z
 draft: false
 ---
 
-## Windows
+## Windows 64-bit
 
 - **Squirrel Installer**  
-  [GB Studio-win32-x64-squirrel-1.0.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-win32-x64-squirrel-1.0.0.zip)  
+  [GB Studio-win32-x64-squirrel-1.1.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-x64-squirrel-1.1.0.zip)  
   _Installs to C:\\ automatically_
 
 - **Manual Install Zip**  
-  [GB Studio-win32-x64-1.0.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-win32-x64-1.0.0.zip)
+  [GB Studio-win32-x64-1.1.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-x64-1.1.0.zip)
+
+## Windows 32-bit
+
+- **Squirrel Installer**  
+  [GB Studio-win32-ia32-squirrel-1.1.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-ia32-squirrel-1.1.0.zip)  
+  _Installs to C:\\ automatically_
+
+- **Manual Install Zip**  
+  [GB Studio-win32-ia32-1.1.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-ia32-1.1.0.zip)
 
 ## MacOS
 
 - **Application Package**  
-  [GB Studio-darwin-x64-1.0.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-darwin-x64-1.0.0.zip)
+  [GB Studio-darwin-x64-1.1.0.zip](https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-darwin-x64-1.1.0.zip)
 
 ## Linux
 
 - **Ubuntu / Debian-based**  
-  [gb-studio_1.0.0_amd64.deb](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/gb-studio_1.0.0_amd64.deb)
+  [gb-studio_1.1.0_amd64.deb](https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/gb-studio_1.1.0_amd64.deb)
 
 - **Fedora / RPM-based**  
-  [gb-studio-1.0.0.x86_64.rpm](https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/gb-studio-1.0.0.x86_64.rpm)
+  [gb-studio-1.1.0.x86_64.rpm](https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/gb-studio-1.1.0.x86_64.rpm)
 
 ## Changelog
 
--- themes/gbstudio/src/js/main.js
+++ themes/gbstudio/src/js/main.js
@@ -17,27 +17,41 @@ if (
 
 const downloadBtn = document.getElementById("download-btn");
 const downloadLink = document.getElementById("download-link");
+const appScreenshot = document.getElementById("app-screenshot");
 
 const downloadUrls = {
   windows:
-    "https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-win32-x64-squirrel-1.0.0.zip",
+    "https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-x64-squirrel-1.1.0.zip",
+  windows32:
+    "https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-ia32-squirrel-1.1.0.zip",
   mac:
-    "https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/GB.Studio-darwin-x64-1.0.0.zip",
+    "https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-darwin-x64-1.1.0.zip",
   deb:
-    "https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/gb-studio_1.0.0_amd64.deb",
+    "https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/gb-studio_1.1.0_amd64.deb",
   rpm:
-    "https://github.com/chrismaltby/gb-studio/releases/download/v1.0.0/gb-studio-1.0.0.x86_64.rpm"
+    "https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/gb-studio-1.1.0.x86_64.rpm"
 };
 
 if (downloadBtn) {
   switch (OSName) {
     case "Windows":
       downloadBtn.innerHTML = "Download for Windows";
-      downloadBtn.href = downloadUrls.windows;
+      appScreenshot.src = "/img/screenshot_win.png";
+      if (
+        navigator.userAgent.indexOf("WOW64") != -1 ||
+        navigator.userAgent.indexOf("Win64") != -1
+      ) {
+        downloadBtn.innerHTML = "Download for Windows 64-bit";
+        downloadBtn.href = downloadUrls.windows;
+      } else {
+        downloadBtn.innerHTML = "Download for Windows 32-bit";
+        downloadBtn.href = downloadUrls.windows32;
+      }
       break;
     case "MacOS":
       downloadBtn.innerHTML = "Download for macOS";
       downloadBtn.href = downloadUrls.mac;
+      appScreenshot.src = "/img/screenshot.png";
       break;
     case "Linux":
       downloadBtn.innerHTML = "Download for Ubuntu";
@@ -47,8 +61,10 @@ if (downloadBtn) {
       redhatDownloadBtn.innerHTML = "Download for Redhat";
       redhatDownloadBtn.href = downloadUrls.rpm;
       downloadBtn.parentNode.insertBefore(redhatDownloadBtn, downloadLink);
+      appScreenshot.src = "/img/screenshot.png";
       break;
     default:
       downloadBtn.innerHTML = "Download GB Studio";
+      appScreenshot.src = "/img/screenshot.png";
   }
 }
